f339ba629ac7ac33ef9cd219c3ee08f3a5a17480
omd24 <jack34169>
2021-10-10 07:38:57 +0330

    updated frame resources
    Added skinned data buffer support as well as SSAO and SMap counterparts

--- character_animation/frame_resource.cpp
+++ character_animation/frame_resource.cpp
@@ -6,8 +6,10 @@ FrameResource::FrameResource (ID3D12Device * dev, UINT pass_cnt, UINT obj_cnt, U
         IID_PPV_ARGS(CmdlistAllocator.GetAddressOf())
     ));
     PassCB = std::make_unique<UploadBuffer<PassConstants>>(dev, pass_cnt, true);
+    SSAOCB = std::make_unique<UploadBuffer<SSAOConstants>>(dev, 1, true);
     MatBuffer = std::make_unique<UploadBuffer<MaterialData>>(dev, mat_cnt, false);
     ObjCB = std::make_unique<UploadBuffer<ObjectConstants>>(dev, obj_cnt, true);
+    SkinnedCB = std::make_unique<UploadBuffer<SkinnedConstants>>(dev, pass_cnt, true);
 }
 FrameResource::~FrameResource () {
 
--- character_animation/frame_resource.h
+++ character_animation/frame_resource.h
@@ -12,6 +12,9 @@ struct ObjectConstants {
     UINT    ObjPad1;
     UINT    ObjPad2;
 };
+struct SkinnedConstants {
+    DirectX::XMFLOAT4X4 BoneTransforms[96];
+};
 struct PassConstants {
     DirectX::XMFLOAT4X4 View = MathHelper::Identity4x4();
     DirectX::XMFLOAT4X4 InvView = MathHelper::Identity4x4();
@@ -19,6 +22,8 @@ struct PassConstants {
     DirectX::XMFLOAT4X4 InvProj = MathHelper::Identity4x4();
     DirectX::XMFLOAT4X4 ViewProj = MathHelper::Identity4x4();
     DirectX::XMFLOAT4X4 InvViewProj = MathHelper::Identity4x4();
+    DirectX::XMFLOAT4X4 ViewProjText = MathHelper::Identity4x4();
+    DirectX::XMFLOAT4X4 ShadowTransform = MathHelper::Identity4x4();
     DirectX::XMFLOAT3 EyePosW = {0.0f, 0.0f, 0.0f};
     float PassPad0;
     DirectX::XMFLOAT2 RenderTargetSize = {0.0f, 0.0f};
@@ -32,24 +37,50 @@ struct PassConstants {
 
     Light Lights[MAX_LIGHTS];
 };
+struct SSAOConstants {
+    DirectX::XMFLOAT4X4 Proj;
+    DirectX::XMFLOAT4X4 InvProj;
+    DirectX::XMFLOAT4X4 ProjTex;
+    DirectX::XMFLOAT4 OffsetVectors[14];
+
+    // -- for ssao_blur.hlsl
+    DirectX::XMFLOAT4 BlurWeights[3];
+
+    DirectX::XMFLOAT2 InvRenderTargetSize = {0.0f, 0.0f};
+
+    float OcclusionRadius = 0.5f;
+    float OcclusionFadeStart = 0.2f;
+    float OcclusionFadeEnd = 2.0f;
+    float SurfaceEpsilon = 0.05f;
+};
 struct MaterialData {
     DirectX::XMFLOAT4 DiffuseAlbedo = {1.0f, 1.0f, 1.0f, 1.0f};
     DirectX::XMFLOAT3 FresnelR0 = {0.01f, 0.01f, 0.01f};
-    float Roughness = 64.0f;
+    float Roughness = 0.5f;
 
     // -- used in texture mapping
     DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
 
     UINT DiffuseMapIndex = 0;
+    UINT NormalMapIndex = 0;
     UINT MatPad0;
     UINT MatPad1;
-    UINT MatPad2;
 };
 struct Vertex {
     DirectX::XMFLOAT3 Pos;
     DirectX::XMFLOAT3 Normal;
     DirectX::XMFLOAT2 TexC;
+    DirectX::XMFLOAT3 TangentU;
 };
+struct SkinnedVertex {
+    DirectX::XMFLOAT3 Pos;
+    DirectX::XMFLOAT3 Normal;
+    DirectX::XMFLOAT2 TexC;
+    DirectX::XMFLOAT3 TangentU;
+    DirectX::XMFLOAT3 BoneWeights;
+    BYTE BoneIndices[4];
+};
+
 class FrameResource
 {
 public:
@@ -64,6 +95,8 @@ public:
     // -- each frame requires its own buffers to separate gpu processing
     std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
     std::unique_ptr<UploadBuffer<ObjectConstants>> ObjCB = nullptr;
+    std::unique_ptr<UploadBuffer<SkinnedConstants>> SkinnedCB = nullptr;
+    std::unique_ptr<UploadBuffer<SSAOConstants>> SSAOCB = nullptr;
     std::unique_ptr<UploadBuffer<MaterialData>> MatBuffer = nullptr;
 
     // -- to check on FrameResource still being used or not
